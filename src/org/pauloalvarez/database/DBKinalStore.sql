4e7c3006632415a5985e599b77556eb11909faf6
Paul Ramos <palvarez-2023238@kinal.edu.gt>
2024-05-22 17:26:00 -0600

    Commit de Seguridad 2?

--- src/org/pauloalvarez/database/DBKinalStore.sql
+++ src/org/pauloalvarez/database/DBKinalStore.sql
@@ -191,15 +191,15 @@ delimiter;
 delimiter $$
 create procedure sp_reporteProveedor()
 begin
-	select Proveedores.codigoProveedor as 'ID',
-    Proveedores.NITProveedor as 'NIT',
-    Proveedores.nombresProveedor as 'Nombres',
-    Proveedores.apellidosProveedor as 'Apellidos',
-    Proveedores.direccionProveedor as 'Dirección',
-    Proveedores.razonSocial as 'Razón Social',
-    Proveedores.contactoPrincipal as 'Contacto Principal',
-    Proveedores.paginaWeb as 'Página Web'
-    from Proveedores;
+	select P.codigoProveedor as 'ID',
+    P.NITProveedor as 'NIT',
+    P.nombresProveedor as 'Nombres',
+    P.apellidosProveedor as 'Apellidos',
+    P.direccionProveedor as 'Dirección',
+    P.razonSocial as 'Razón Social',
+    P.contactoPrincipal as 'Contacto Principal',
+    P.paginaWeb as 'Página Web'
+    from Proveedores P;
 end$$
 delimiter;
 
@@ -212,14 +212,16 @@ create procedure sp_agregarTelProveedor(in _codigoTelefonoProveedor int,
 											_observaciones varchar(45),
 											_codigoProveedor int)
 begin
-	insert into TelProveedor (codigo)
+	insert into TelProveedor (codigoTelefonoProveedor, numeroPrincipal, numeroSecundario, observaciones, codigoProveedor)
+    values (_codigoTelefonoProveedor, _numeroPrincipal, _numeroSecundario, _observaciones, _codigoProveedor);
 end$$
 delimiter;
 
 delimiter $$
 create procedure sp_eliminarTelProveedor(in codigoTelefonoProveedor int)
 begin
-	
+	delete from TelProveedor
+    where codigoTelProveedor = _codigoTelProveedor;
 end$$
 delimiter;
 
@@ -230,97 +232,159 @@ create procedure sp_editarTelProveedor(in _codigoTelefonoProveedor int,
 											_observaciones varchar(45),
 											_codigoProveedor int)
 begin
-	
+	update TelProveedor
+	set numeroPrincipal = _numeroPrincipal,
+    numeroSecundario = _numeroSecundario,
+    observaciones = _observaciones,
+    codigoProveedor = _codigoProveedor
+    where codigoTelProveedor = codigoTelProveedor;
 end$$
 delimiter;
 
 delimiter $$
 create procedure sp_reporteTelProveedor()
 begin
-	
+	select TP.codigoTelProveedor as 'ID',
+    TP.numeroPrincipal as 'No. Principal',
+    TP.numeroSecundario as 'No. Secundario',
+    TP.observaciones as 'Observaciones',
+    TP.codigoProveedor as 'Proveedor'
+    from TelProveedor TP;
 end$$
 delimiter;
 
 -- CRUD Email Proveedores --
 
 delimiter $$
-create procedure sp_agregarEmailProveedor(in )
+create procedure sp_agregarEmailProveedor(in _codigoEmailProveedor int,
+												_emailProveedor varchar(50),
+												_descripcion varchar(100),
+												_codigoProveedor int)
 begin
-	
+	insert into EmailProveedor (codigoEmailProveedor, emailProveedor, descripcion, codigoProveedor)
+    values (_codigoEmailProveedor, _emailProveedor, _descripcion, _codigoProveedor);
 end$$
 delimiter;
 
 delimiter $$
-create procedure sp_eliminarEmailProveedor(in )
+create procedure sp_eliminarEmailProveedor(in _codigoEmailProveedor int)
 begin
-	
+	delete from EmailProveedor
+    where codigoEmailProveedor = _codigoEmailProveedor;
 end$$
 delimiter;
 
 delimiter $$
-create procedure sp_editarEmailProveedor(in )
+create procedure sp_editarEmailProveedor(in _codigoEmailProveedor int,
+											_emailProveedor varchar(50),
+											_descripcion varchar(100),
+											_codigoProveedor int)
 begin
-	
+	update EmailProveedor
+    set emailProveedor = _emailProveedor,
+    descripcion = _descripcion,
+    codigoProveedor = _codigoProveedor
+    where codigoEmailProveedor = _codigoEmailProveedor;
 end$$
 delimiter;
 
 delimiter $$
 create procedure sp_reporteEmailProveedor()
 begin
-	
+	select EP.codigoEmailProveedor as 'ID',
+    EP.emailProveedor as 'E-Mail',
+    EP.descripcion as 'Descripción',
+    EP.codigoProveedor as 'Proveedor'
+    from EmailProveedor EP;
 end$$
 delimiter;
 
-CRUD Empleados 
+/* CRUD Empleados */ 
 
 delimiter $$
-create procedure sp_agregarEmpleado(in )
+create procedure sp_agregarEmpleado(in _codigoEmpleado int,
+										_nombresEmpleado varchar(50),
+										_apellidosEmpleado varchar(50),
+										_sueldo decimal(10,2),
+										_direccion varchar(150),
+										_turno varchar(15),
+                                        _cargoEmpleado int)
 begin
-	
+	insert into Empleados (codigoEmpleado, nombresEmpleado, apellidosEmpleado, sueldo, direccion, turno, cargoEmpleado)
+    values (_codigoEmpleado, _nombresEmpleado, _apellidosEmpleado, _sueldo, _direccion, _turno, _cargoEmpleado);
 end$$
 delimiter;
 
 delimiter $$
-create procedure sp_eliminarEmpleado(in )
+create procedure sp_eliminarEmpleado(in _codigoEmpleado int)
 begin
-	
+	delete from Empleados
+    where codigoEmpleado = _codigoEmpleado;
 end$$
 delimiter;
 
 delimiter $$
-create procedure sp_editarEmpleado(in )
+create procedure sp_editarEmpleado(in _codigoEmpleado int,
+										_nombresEmpleado varchar(50),
+										_apellidosEmpleado varchar(50),
+										_sueldo decimal(10,2),
+										_direccion varchar(150),
+										_turno varchar(15),
+                                        _cargoEmpleado int)
 begin
-	
+	update Empleados
+    set nombresEmpleado = _nombresEmpleado,
+    apellidosEmpleado = _apellidosEmpleado,
+    sueldo = _sueldo,
+    direccion = _direccion,
+    turno = _turno,
+    cargo = _cargo;
 end$$
 delimiter;
 
 delimiter $$
 create procedure sp_reporteEmpleado()
 begin
-	
+	select E.codigoEmpleado as 'ID',
+    E.nombresEmpleado as 'Nombres',
+    E.apellidosEmpleado as 'Apellidos',
+    E.sueldo as 'Sueldo',
+    E.direccion = direccion,
+    E.turno as 'turno',
+    E.cargoEmpleado as 'Puesto'
+    from Empleados E;
 end$$
 delimiter;
 
 -- CRUD Cargo Empleado --
 
 delimiter $$
-create procedure sp_agregarCargoEmpleado(in )
+create procedure sp_agregarCargoEmpleado(in _codigoCargoEmpleado int,
+											_nombreCargo varchar(45),
+											_descripcionCargo varchar(45))
 begin
-	
+	insert into CargoEmpleado (codigoCargoEmpleado, nombreCargo, descripcionCargo)
+    values (_codigoCargoEmpleado, _nombreCargo, _descripcionCargo);
 end$$
 delimiter;
 
 delimiter $$
-create procedure sp_eliminarCargoEmpleado(in )
+create procedure sp_eliminarCargoEmpleado(in _codigoCargoEmpleado int)
 begin
-	
+	delete from CargoEmpleado
+    where codigoCargoEmpleado = _codigoCargoEmpleado;
 end$$
 delimiter;
 
 delimiter $$
-create procedure sp_editarCargoEmpleado(in )
+create procedure sp_editarCargoEmpleado(in _codigoCargoEmpleado int,
+											_nombreCargo varchar(45),
+											_descripcionCargo varchar(45))
 begin
-	
+	select CE.codigoCargoEmpleado as 'ID',
+    CE.nombreCargo as 'Cargo',
+    CE.descripcionCargo as 'Descipción'
+    from CargoEmpleado CE;
 end$$
 delimiter;
 
@@ -378,14 +442,14 @@ delimiter ;
 delimiter $$
 create procedure sp_reporteCliente()
 begin
-	select Clientes.codigoCliente as 'ID',
-    Clientes.NITCliente as 'NIT',
-    Clientes.nombresCliente as 'Nombres',
-    Clientes.apellidosCliente as 'Apellidos',
-    Clientes.direccionCliente as 'Dirección',
-    Clientes.telefonoCliente as 'Teléfono',
-    Clientes.emailCliente as 'E-mail'
-    from Clientes;
+	select C.codigoCliente as 'ID',
+    C.NITCliente as 'NIT',
+    C.nombresCliente as 'Nombres',
+    C.apellidosCliente as 'Apellidos',
+    C.direccionCliente as 'Dirección',
+    C.telefonoCliente as 'Teléfono',
+    C.emailCliente as 'E-mail'
+    from Clientes C;
 end$$
 delimiter;
 
@@ -439,15 +503,15 @@ delimiter;
 delimiter $$
 create procedure sp_reporteProducto()
 begin
-	select Productos.codigoProducto as 'ID',
-    Productos.descripcionProducto as 'Descripción',
-    Productos.precioUnitario as 'Precio/Unidad',
-    Productos.precioDocena as 'Precio/Docena',
-    Productos.precioMayor as 'Precio/Mayor',
-    Productos.cantidadExistencia as 'Cant. Existencia',
-    Productos.codigoTipoProducto as 'Categoría ID',
-    Productos.codigoProveedor as 'Proveedor ID'
-    from Productos;
+	select Pr.codigoProducto as 'ID',
+    Pr.descripcionProducto as 'Descripción',
+    Pr.precioUnitario as 'Precio/Unidad',
+    Pr.precioDocena as 'Precio/Docena',
+    Pr.precioMayor as 'Precio/Mayor',
+    Pr.cantidadExistencia as 'Cant. Existencia',
+    Pr.codigoTipoProducto as 'Categoría ID',
+    Pr.codigoProveedor as 'Proveedor ID'
+    from Productos Pr;
 end$$
 delimiter;
 
@@ -481,9 +545,9 @@ delimiter;
 delimiter $$
 create procedure sp_reporteTipoProducto()
 begin
-	select TipoProdcuto.codigoTipoProducto as 'ID',
-    TipoProducto.descripcion as 'Descripción'
-    from TipoProducto;
+	select TPr.codigoTipoProducto as 'ID',
+    TPr.descripcion as 'Descripción'
+    from TipoProducto TPr;
 end$$
 -- delimiter;
 
